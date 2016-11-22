f785d3364c4bca1e9166287080c15ffd112cc1f9
freedomyy <freedomyy27@gmail.com>
2016-11-22 11:19:06 -0800

    cross validation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import csv
 from sklearn import svm
+from sklearn.model_selection import cross_val_score
 
 
 with open('train.csv', 'rb') as csvfile:
@@ -8,6 +9,9 @@ with open('train.csv', 'rb') as csvfile:
   data = [row for row in datareader]
   features = [row[:-1] for row in data]
   labels = [row[-1][-1] for row in data]
+  
 clf = svm.SVC(gamma=0.001, C=100.)
 clf.fit(features[:-1], labels[:-1])
-print clf.predict(features[-1])
+
+scores = cross_val_score(clf,features,labels,cv=5)
+print scores
