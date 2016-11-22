6363b0e5ed360a27f8ad61768121e201899864c5
freedomyy <freedomyy27@gmail.com>
2016-11-22 12:53:32 -0800

    decision tree

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import csv
 from sklearn import svm
+from sklearn import tree
 from sklearn.model_selection import cross_val_score
 
 
@@ -7,11 +8,14 @@ with open('train.csv', 'rb') as csvfile:
   datareader = csv.reader(csvfile, dialect="excel")
   datareader.next()
   data = [row for row in datareader]
-  features = [row[:-1] for row in data]
+  features = [row[1:-1] for row in data]
   labels = [row[-1][-1] for row in data]
-  
-clf = svm.SVC(gamma=0.001, C=100.)
-clf.fit(features[:-1], labels[:-1])
 
-scores = cross_val_score(clf,features,labels,cv=5)
+clf = svm.SVC(gamma=0.001, C=100.)
+clf.fit(features, labels)
+
+clf1 = tree.DecisionTreeClassifier()
+clf1.fit(features,labels)
+
+scores = cross_val_score(clf1,features,labels,cv=5)
 print scores
