f9d6a43d63bd02259263bcd828af8e62282d745d
Tim <zrccxyb62@gmail.com>
2016-11-23 17:18:07 -0800

    cross validate using the best parameters

--- main.py
+++ main.py
@@ -7,12 +7,13 @@ from sklearn.model_selection import GridSearchCV
 from sklearn import preprocessing
 import numpy as np
 
-def test_svm(ft, lbl):
-    param_grid = [{'C': [1, 10, 100, 1000], 'kernel': ['linear']},{'C': [1, 10, 100, 1000], 'gamma': [0.01, 0.001, 0.0001], 'kernel': ['rbf']}]
-    cv = ShuffleSplit(n_splits=5, test_size=0.2, train_size=0.2, random_state=0)
+def get_best_svm_param(ft, lbl):
+    param_grid = [{'C': [1, 10, 100], 'kernel': ['linear']},{'C': [1, 10, 100, 1000], 'gamma': [0.01, 0.001, 0.0001], 'kernel': ['rbf']}]
+    cv = ShuffleSplit(n_splits=4, test_size=0.15, train_size=0.15, random_state=0)
     clf_svm = svm.SVC()
     gs = GridSearchCV(clf_svm, param_grid, cv=cv, n_jobs=-1, verbose=2)
     gs.fit(ft, lbl)
+    return gs.best_estimator_, gs.best_params_, gs.best_score_
 
 if __name__ == "__main__":
     with open('train.csv', 'rb') as csvfile:
@@ -22,11 +23,14 @@ if __name__ == "__main__":
         features = preprocessing.scale(np.array([row[1:-1] for row in data]))
         labels = np.array([row[-1][-1] for row in data])
 
-    test_svm(features, labels)
+    results = get_best_svm_param(features, labels)
+    print results
+    best_param = results[1]
 
-    clf_svm = svm.SVC(gamma=0.01, C=10.)
+    print 'Start on the whole training set...'
+    clf_svm = svm.SVC(kernel = best_param['kernel'], gamma = best_param['gamma'], C = best_param['C'])
     clf_svm.fit(features, labels)
-    print cross_val_score(clf_svm, features, labels, cv=5)
+    print cross_val_score(clf_svm, features, labels, cv = 5)
     #clf_tree = tree.DecisionTreeClassifier()
     #clf_tree.fit(features,labels)
 
