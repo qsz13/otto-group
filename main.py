d7acd21735f9179fa0a052e66b9554274a354047
Tim <zrccxyb62@gmail.com>
2016-11-28 19:56:40 -0800

    add formatted predicted output

--- main.py
+++ main.py
@@ -52,28 +52,35 @@ if __name__ == "__main__":
     #    labels = np.array([row[-1][-1] for row in data])
 
     # Import Data
+    tests = pd.read_csv('test.csv')
+    tests = tests.drop('id', axis=1)
+    scaler = StandardScaler(copy=False, with_mean=True, with_std=True)
+    tests = scaler.fit_transform(tests)
+    #print tests
+    
     #features = pd.read_csv('../input/train.csv')
     features = pd.read_csv('train.csv')
     features = features.drop('id', axis=1)
-
+    
     # Extract target and Encode it to make it manageable by ML algo
     labels = features.target.values
     labels = LabelEncoder().fit_transform(labels)
-
+    #print labels
+    
     # Remove target from train, else it's too easy ...
     features = features.drop('target', axis=1)
 
     #features = preprocessing.normalize(features)
     scaler = StandardScaler(copy=False, with_mean=True, with_std=True)
     features = scaler.fit_transform(features, labels)
-    print features
+    #print features
     #minmax scalar
     #min_max_scaler = preprocessing.MinMaxScaler()
     #features = min_max_scaler.fit_transform(features)
     #print features
 
     # svm 81.5%
-    get_best_svm_param(features, labels)
+    #get_best_svm_param(features, labels)
 
 
     # decision tree 70%
@@ -86,9 +93,11 @@ if __name__ == "__main__":
     #features, features_test, labels, labels_test = train_test_split(features, labels, test_size=0.20, random_state=36)
 
     # calibrated random forest 81.6%
-    #clf = RandomForestClassifier(n_estimators=50, random_state=1337, n_jobs=-1)
-    #calibrated_clf = CalibratedClassifierCV(clf, method='isotonic', cv=5)
-    #calibrated_clf.fit(features, labels)
+    clf = RandomForestClassifier(n_estimators=50, random_state=1337, n_jobs=-1)
+    calibrated_clf = CalibratedClassifierCV(clf, method='isotonic', cv=5)
+    calibrated_clf.fit(features, labels)
+    predicted_labels = calibrated_clf.predict(tests)
+    len_lbl = len(predicted_labels)
     #print cross_val_score(calibrated_clf, features, labels, cv=5)
     #ypreds = calibrated_clf.predict_proba(features_test)
     #print("logloss with calibration : ", log_loss(labels_test, ypreds, eps=1e-15, normalize=True))
@@ -104,3 +113,24 @@ if __name__ == "__main__":
     #clf_mlp = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes = (25,), random_state = 1)
     #clf_mlp.fit(features, labels)
     #print cross_val_score(clf_mlp, features, labels, cv=5)
+    
+    output_mtx = np.zeros((len_lbl,10),dtype=np.uint32)
+    for i in xrange(len_lbl):
+        output_mtx[i,0] = i + 1
+        output_mtx[i,predicted_labels[i]+1] = 1
+    #print output_mtx[0]
+    #print output_mtx[144367]
+    raw_data = {'id': output_mtx[:,0],
+        'class_1': output_mtx[:,1],
+        'class_2': output_mtx[:,2],
+        'class_3': output_mtx[:,3],
+        'class_4': output_mtx[:,4],
+        'class_5': output_mtx[:,5],
+        'class_6': output_mtx[:,6],
+        'class_7': output_mtx[:,7],
+        'class_8': output_mtx[:,8],
+        'class_9': output_mtx[:,9]
+        }
+    df = pd.DataFrame(raw_data, columns = ['id', 'class_1', 'class_2', 'class_3', 'class_4', 'class_5', 'class_6', 'class_7', 'class_8', 'class_9'])
+    df.to_csv('result.csv',index = False, index_label =False)
+    
